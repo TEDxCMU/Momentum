76c140a21db09b2a196409c17e5d6d86243566d5
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-03-28 21:24:12 -0400

    tweak about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,40 +1,47 @@
 import Link from 'next/link';
-import styles from 'components/about.module.css';
 import Twitter from 'assets/twitter.svg';
 import Linkedin from 'assets/linkedin.svg';
-import Blob from 'assets/aboutBlob.svg'
+import styles from 'components/about.module.css';
 
 function About() {
     return (
         <main className={styles.container}>
-            <h1 className={styles.header}>About</h1>
-
-            <section>
-                <div className={styles.cardWrapper}>
-                    <div className={styles.aboutText}>
-                        <p>Momentum is what drives something still into an unstoppable force of motion, power, energy and most importantly, change. It fuels motivation, defies stagnancy and demands the spurring of action. People who are constantly in momentum are those that make change, those that pursue something more and those that see in the world the potential for something better.</p>
-                        <p>Momentum pushes us to keep moving in spite of the challenges we face. TEDxCMU Momentum hopes to teach the power of striving for more and taking the step that allows momentum to thrive—the first one forward.</p>
-                        <div className={styles.links}>
-                            <div className={styles.tedButton}>
-                                <Link href="https://www.tedxcmu.org/">
-                                    <a>
-                                        TEDxCMU.ORG
-                                    </a>
-                                </Link>
-                            </div>
-                            <div className={styles["socials-wrapper"]}>
-                                <Link href="https://twitter.com/tedxcmu"><a><Twitter /></a></Link>
-                                <Link href="https://www.linkedin.com/company/tedxcmu/"><a><Linkedin /></a></Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.aboutImg}>
-                        <img src='tedxcmu-logo.svg'></img>
-                        {/*<Blob />  can't figure out how to put in blob svg as part of the bg*/}
+            <h1 className={styles.header}>
+                About
+            </h1>
+            <section className={styles.card}>
+                <div className={styles.content}>
+                    <p className={styles.text}>
+                        Momentum is what drives something still into an unstoppable force of motion, power, energy and most importantly, change. It fuels motivation, defies stagnancy and demands the spurring of action. People who are constantly in momentum are those that make change, those that pursue something more and those that see in the world the potential for something better.
+                    </p>
+                    <p className={styles.text}>
+                        Momentum pushes us to keep moving in spite of the challenges we face. TEDxCMU Momentum hopes to teach the power of striving for more and taking the step that allows momentum to thrive—the first one forward.
+                    </p>
+                    <div className={styles.links}>
+                        <Link href="https://www.tedxcmu.org">
+                            <a className={styles.button} target="_blank" rel="noopener noreferrer">
+                                TEDxCMU.ORG
+                            </a>
+                        </Link>
+                        <Link href="https://twitter.com/tedxcmu">
+                            <a className={styles.icon} target="_blank" rel="noopener noreferrer">
+                                <Twitter />
+                            </a>
+                        </Link>
+                        <Link href="https://www.linkedin.com/company/tedxcmu">
+                            <a target="_blank" rel="noopener noreferrer">
+                                <Linkedin />
+                            </a>
+                        </Link>
                     </div>
                 </div>
-
+                <div>
+                    <img
+                        className={styles.image}
+                        src="assets/about.svg"
+                        alt="TEDxCMU Momentum Graphic"
+                    />
+                </div>
             </section>
         </main>
     );
