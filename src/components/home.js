1022d97813ff8e9bb070d4eead3752d8a24b06d7
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-03-29 11:28:05 -0400

    add history stage to home graphic to prevent duplicates on refresh and click

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
-import styles from 'components/home.module.css'
 import { useEffect, useRef, useState } from 'react';
+import useLocalStorage from 'hooks/useLocalStorage';
+import styles from 'components/home.module.css'
 
 import HomeGraphic1 from 'assets/home/home-1.svg';
 import HomeGraphic2 from 'assets/home/home-2.svg';
@@ -10,22 +11,37 @@ import HomeGraphic6 from 'assets/home/home-6.svg';
 
 function Home() {
     const containerRef = useRef(null);
-    const [imageIndex, setImageIndex] = useState(null);
+    const [imageIndex, setImageIndex] = useLocalStorage('momentum_image_index', Math.floor(Math.random() * 6) + 1);
 
     // Width, Height, Top, Left
     const dimensions = [[2289.43, 1797.36, 142.06, 2223.4], [1263.81, 1486.9, -427.26, 336.84], [1226.88, 869.04, 177.09, -50], [1180.37, 1041.85, 1011.36, 324.88], [1448.71, 1722.85, 507.03, 1452.29], [1297.62, 1157.84, -545, 471.65]];
     const colors = ['#C1FFBF', '#FF2F54', '#FF8A00', '#4A5CFF', '#E370DF'];
 
+    let pastColor;
+
     function handleColorChange(event) {
         const paths = containerRef.current.querySelectorAll('path');
-        const color = colors[Math.floor(Math.random() * colors.length)];
+
+        let newColor = pastColor;
+        do {
+            newColor = Math.floor(Math.random() * colors.length);
+        } while (newColor === pastColor);
+
+        pastColor = newColor;
+
         for (const path of paths) {
-            path.setAttribute('stroke', color);
+            path.setAttribute('stroke', colors[pastColor]);
         }
     }
 
     useEffect(() => {
-        setImageIndex(Math.floor(Math.random() * 6) + 1);
+        let newImageIndex = imageIndex;
+
+        do {
+            newImageIndex = Math.floor(Math.random() * 6) + 1;
+        } while (newImageIndex === imageIndex);
+
+        setImageIndex(newImageIndex);
 
         document.body.addEventListener('click', handleColorChange, true);
 
